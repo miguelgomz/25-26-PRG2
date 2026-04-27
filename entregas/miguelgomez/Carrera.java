31ad0cb183c0c9042ce5c3b91afd10158cfd2257
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-27 20:49:43 +0200

    realciono la clase caballo y clase pista para que lo controle clase carrera

--- entregas/miguelgomez/Carrera.java
+++ entregas/miguelgomez/Carrera.java
@@ -1,6 +1,13 @@
 public class Carrera {
+  private Caballo[] caballos;
+    private Pista pista;
+
     public Carrera() {
-        
+        this.pista = new Pista(40);
+        this.caballos = new Caballo[2];
+        for (int i = 0; i < caballos.length; i++) {
+            caballos[i] = new Caballo(i + 1);
+        }
     }
     public void jugar() {
         
