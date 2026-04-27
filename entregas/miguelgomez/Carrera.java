406a5bde333d1003cf1ebdac1c7a4f12903274e2
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-27 20:55:02 +0200

    calcular ganador y ver el manejo de los empates

--- entregas/miguelgomez/Carrera.java
+++ entregas/miguelgomez/Carrera.java
@@ -26,4 +26,21 @@ public class Carrera {
         pista.mostrarPista(caballos);
         mostrarResultadoFinal();
     }
+    public void jugar() {
+        boolean carreraTerminada = false;
+
+        while (!carreraTerminada) {
+            pista.mostrarPista(caballos);
+
+            for (Caballo caballo : caballos) {
+                caballo.correr(pista.getLongitud());
+                if (caballo.getAvance() >= pista.getLongitud() - 1) {
+                    carreraTerminada = true;
+                }
+            }
+        }
+
+        pista.mostrarPista(caballos);
+        mostrarResultadoFinal();
+    }
  
