407c174df1eac99753b7a9c99caa096546663e77
miguelgomz <miguel.gomez1@alumnos.uneatlantico.es>
2026-04-27 20:48:07 +0200

    esta clase sera la encargada de dibujar el juego en la pantalla,asi clase caballo solo se fija en avanzar

--- entregas/miguelgomez/Pista.java
+++ entregas/miguelgomez/Pista.java
@@ -4,4 +4,24 @@ public class Pista {
     public Pista(int longitud) {
         this.longitud = longitud;
     }
+    public void mostrarPista(Caballo[] caballos) {
+        System.out.println("----------------------------------------------");
+        for (Caballo caballo : caballos) {
+            StringBuilder carril = new StringBuilder(); 
+            for (int i = 0; i < longitud; i++) {
+                if (i == caballo.getAvance()) {
+                    carril.append(";-;'");
+                } else {
+                    carril.append(" ");
+                }
+            }
+            System.out.println(carril.toString());
+        }
+        System.out.println("----------------------------------------------");
+    }
+
+    public int getLongitud() { 
+        return this.longitud; 
+    }
+}
 
